da00ff99c937d7e9b6316ede6ee6475a94cc44cf
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2013-02-20 23:08:30 +0000

    Atualizações

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -4,7 +4,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.5';
+$wp_version = '3.5.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
@@ -18,7 +18,7 @@ $wp_db_version = 22441;
  *
  * @global string $tinymce_version
  */
-$tinymce_version = '358-23156';
+$tinymce_version = '358-23224';
 
 /**
  * Holds the required PHP version
@@ -33,3 +33,5 @@ $required_php_version = '5.2.4';
  * @global string $required_mysql_version
  */
 $required_mysql_version = '5.0';
+
+$wp_local_package = 'pt_BR';
